4ad1e6a95d516479bb4bb11042ebd59d397b8899
kenkatou354 <kenkatou354@gmail.com>
2025-12-10 13:54:56 +0900

    [add]ゲームマネージャークラスを追加

---
+++ My project/Assets/Player/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   
+    private void Awake()
+    {
+        
+    }
+    private void GetComponent()
+    {
+      
+    }
+    private void Init()
+    {
+
+    }
+
+
+}
